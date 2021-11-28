59fa96841966d1998a936f55e679785ff32e3113
jhuffaker13 <91619777+jhuffaker13@users.noreply.github.com>
2021-11-27 23:44:03 -0700

    messing with weapon.py

--- finale/game/weapon.py
+++ finale/game/weapon.py
@@ -3,6 +3,7 @@ from game import constants
 from game.action import Action
 from game.point import Point
 from game.hero import Hero
+import random
 
 
 class Weapon():
@@ -54,3 +55,19 @@ class Weapon():
                     
                     lasers.remove(laser)
                     break
+
+        """enemies = cast["enemies"]
+        for enemy in enemies:
+            random_shot = random.randint(1, 1000)
+            if random_shot == 1:
+                laser = Actor()
+                enemy_x = enemy.get_position().get_x()
+                enemy_y = enemy.get_position().get_y()
+                position = Point(enemy_x - 10, enemy_y)
+                laser.set_image(constants.IMAGE_BALL)
+                laser.set_height(constants.HERO_HEIGHT)
+                laser.set_width(constants.HERO_WIDTH)
+                laser.set_position(position)
+                laser.set_velocity(Point(-5, 0))
+                self.laserlist.append(laser)
+                cast["enemyweapon"] = self.laserlist"""
